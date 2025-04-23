bc4edcd6c86338f38cd127460528685316bfd845
MounssifCharifi <MoncefCharifi2000@gmail.com>
2025-04-23 20:48:14 +0100

    Diviser le component offers.jsx

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -4,9 +4,8 @@ import ProfilCandidat from "../features/candidat/pages/Profil/ProfilCandidat";
 import Navbar from "../features/candidat/components/navbar/Navbar";
 import Footer from "../features/candidat/components/footer/Footer";
 import Candidature from "../features/candidat/pages/Candidature";
-import Services from "../features/candidat/pages/Services";
 import Contact from "../features/candidat/pages/Contact";
-import Offers from "../features/candidat/pages/Offers";
+import Offers from "../features/candidat/pages/offers/Offers";
 import About from "../features/candidat/pages/About";
 import Home from "../features/candidat/pages/Home";
 import { GuardedRoute } from "../middleware/Guards";
@@ -22,7 +21,6 @@ function PublicLayout() {
         <Route path="/about" element={<About />} />
         <Route path="/offers" element={<Offers />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/services" element={<Services />} />
         <Route  path="/candidature/:id" element={<Candidature />}/>
 
         {/* 🔐 Routes protégées */}
